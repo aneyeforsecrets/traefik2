27dcf9e8e65fd7c75a60987e44842c01ebf898fd
Dennis Irrgang <d@irrgang.dev>
2021-05-13 18:57:15 +0200

    smokeping setup

---
+++ librenms/config/config2.php
@@ -0,0 +1,5 @@
+<!-- $config['smokeping']['dir'] = '/var/lib/smokeping';
+$config['smokeping']['pings'] = 20;
+$config['smokeping']['probes'] = 2;
+$config['smokeping']['integration'] = true;
+$config['smokeping']['url'] = 'smokeping/';  // If you have a specific URL or path for smokeping -->
